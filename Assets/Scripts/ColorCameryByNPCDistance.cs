ed6a316c69ebeaa4d11c2606d7943be9f0d121a2
Babsi <barbara.goebl@gmail.com>
2015-12-09 09:00:53 +0100

    setting up scripts to color the Camera in NPC halo

---
+++ Assets/Scripts/ColorCameryByNPCDistance.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorCameryByNPCDistance : MonoBehaviour {
+    public Transform colorPlane;
+
+    private Material planeMaterial;
+    private Color color;
+
+	// Use this for initialization
+	void Start () {
+
+        //colorPlane.transform.LookAt(GetComponent<Camera>().transform.position);
+        color = colorPlane.GetComponent<Renderer>().material.color;
+        color.a = 0.5f;
+        colorPlane.GetComponent<Renderer>().material.color = color;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
